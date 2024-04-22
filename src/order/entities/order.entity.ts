7b796b27a7f867905922c25143395f64da835bd0
Emna Dammak <emna.dammak@insat.ucar.tn>
2024-04-22 11:52:25 +0100

    fixed on delete cascade

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,28 +1,33 @@
-import { Column, Entity, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { UserEntity } from '../../user/entities/user.entity';
-import { ServiceEntity } from '../../service/entities/service.entity';
 import { OrderServiceEntity } from '../../order-service/entities/order-service.entity';
 import { TimestampEntity } from '../../generics/timestamp.entity';
 
 @Entity('order')
-export class OrderEntity extends TimestampEntity{
-
+export class OrderEntity extends TimestampEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column({
-    nullable: false
+    nullable: false,
   })
   finalPrice: number;
 
-
-  @ManyToOne(() => UserEntity, user => user.orders,{eager:true,nullable:false})
+  @ManyToOne(() => UserEntity, (user) => user.orders, {
+    eager: true,
+    nullable: false,
+  })
   user: UserEntity;
 
-  @OneToMany(()=>OrderServiceEntity, orderService => orderService.order,{ cascade: true, onDelete: 'CASCADE' })
+  @OneToMany(() => OrderServiceEntity, (orderService) => orderService.order, {
+
+  })
   orderServices: OrderServiceEntity[];
-
-
-
-
 }
