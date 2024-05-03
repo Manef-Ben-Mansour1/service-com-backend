37daea04d455832598f91adf7ce533c3a7a40268
AzizBenDhiab <mohamedaziz.bendhiab@insat.ucar.tn>
2024-05-03 09:43:35 +0100

    chat gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,45 +1,68 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import * as process from 'node:process';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import * as dotenv from 'dotenv';
-import * as process from "node:process";
-import { UserEntity } from './user/entities/user.entity';
-import { TimestampEntity } from './generics/timestamp.entity';
 import { CategoryModule } from './category/category.module';
 import { CategoryEntity } from './category/entities/category.entity';
-import { ServiceModule } from './service/service.module';
-import { ServiceEntity } from './service/entities/service.entity';
-import { OrderModule } from './order/order.module';
-import { OrderEntity } from './order/entities/order.entity';
-import { OrderServiceModule } from './order-service/order-service.module';
-import { OrderServiceEntity } from './order-service/entities/order-service.entity';
-import { ProfessionModule } from './profession/profession.module';
-import { ProfessionEntity } from './profession/entities/profession.entity';
 import { CommentModule } from './comment/comment.module';
 import { CommentEntity } from './comment/entities/comment.entity';
-import { RatingModule } from './rating/rating.module';
-import { RatingEntity } from './rating/entities/rating.entity';
-import { MessageModule } from './message/message.module';
-import { ConversationEntity } from './conversation/entities/conversation.entity';
 import { ConversationModule } from './conversation/conversation.module';
+import { ConversationEntity } from './conversation/entities/conversation.entity';
 import { MessageEntity } from './message/entities/message.entity';
+import { MessageModule } from './message/message.module';
+import { OrderServiceEntity } from './order-service/entities/order-service.entity';
+import { OrderServiceModule } from './order-service/order-service.module';
+import { OrderEntity } from './order/entities/order.entity';
+import { OrderModule } from './order/order.module';
+import { ProfessionEntity } from './profession/entities/profession.entity';
+import { ProfessionModule } from './profession/profession.module';
+import { RatingEntity } from './rating/entities/rating.entity';
+import { RatingModule } from './rating/rating.module';
+import { ServiceEntity } from './service/entities/service.entity';
+import { ServiceModule } from './service/service.module';
+import { UserEntity } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
+import { MessagesGateway } from './chat/chat.gateway';
 
 dotenv.config();
 
 @Module({
-  imports: [ TypeOrmModule.forRoot({
-    type: "mysql",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [UserEntity,CategoryEntity,ProfessionEntity,ServiceEntity,OrderEntity,OrderServiceEntity,CommentEntity,RatingEntity,MessageEntity,ConversationEntity],
-    synchronize: true,
-  }), UserModule, CategoryModule, ServiceModule, OrderModule, OrderServiceModule, ProfessionModule, CommentModule, RatingModule, MessageModule, ConversationModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [
+        UserEntity,
+        CategoryEntity,
+        ProfessionEntity,
+        ServiceEntity,
+        OrderEntity,
+        OrderServiceEntity,
+        CommentEntity,
+        RatingEntity,
+        MessageEntity,
+        ConversationEntity,
+      ],
+      synchronize: true,
+    }),
+    UserModule,
+    CategoryModule,
+    ServiceModule,
+    OrderModule,
+    OrderServiceModule,
+    ProfessionModule,
+    CommentModule,
+    RatingModule,
+    MessageModule,
+    ConversationModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MessagesGateway],
 })
 export class AppModule {}
--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,60 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { MessageEntity } from '../message/entities/message.entity';
+import { CreateMessageDto } from '../message/dto/create-message.dto';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { MessageService } from '../message/message.service';
 
-@WebSocketGateway()
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class MessagesGateway {
+  constructor(private readonly messageService: MessageService) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('connect')
+  handleConnection(@ConnectedSocket() client: Socket): string {
+    console.log('connected');
+    client.join('3');
+    console.log('connected');
+
+    return 'connected';
+  }
+
+  @SubscribeMessage('allMessages')
+  async getAllMessages(
+    @ConnectedSocket() client: Socket,
+  ): Promise<MessageEntity[]> {
+    let messages = await this.messageService.findAll();
+    for (const item of messages) {
+      console.log(item.text);
+    }
+    return this.messageService.findAll();
+  }
+
+  @SubscribeMessage('addMessage')
+  async addMessage(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: string,
+  ): Promise<MessageEntity> {
+    const createMessageDto: CreateMessageDto = JSON.parse(data);
+    console.log(createMessageDto.recipientId);
+    console.log(createMessageDto.senderId);
+
+    const newMessage = await this.messageService.create(createMessageDto);
+    this.server
+      .to(createMessageDto.recipientId.toString())
+      .emit('message', newMessage);
+    return newMessage;
   }
 }
