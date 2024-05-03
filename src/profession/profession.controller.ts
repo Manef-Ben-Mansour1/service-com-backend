f450f47c16a4814374fe3b675afdb5212c869b41
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2024-05-03 21:54:14 +0100

    adding route guard

--- src/profession/profession.controller.ts
+++ src/profession/profession.controller.ts
@@ -1,27 +1,31 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { ProfessionService } from './profession.service';
 import { CreateProfessionDto } from './dto/create-profession.dto';
 import { ProfessionEntity } from './entities/profession.entity';
 import { UpdateProfessionDto } from './dto/update-profession.dto';
+import { JwtAuthGuard } from '../user/guards/jwt-auth.guard';
+import { AdminGuard } from '../user/guards/admin.guard';
 
 
 @Controller('profession')
 export class ProfessionController {
   constructor(private  readonly  professionService:ProfessionService) {}
+  @UseGuards(JwtAuthGuard, AdminGuard)
  @Post()
   async create(@Body() createProfessionDto: CreateProfessionDto): Promise<ProfessionEntity> {
     return this.professionService.createProfession(createProfessionDto);
   }
-
+  @UseGuards(JwtAuthGuard, AdminGuard)
   @Patch(':id')
   async update(@Param('id',ParseIntPipe) id: number,@Body() updateProfessionDto: UpdateProfessionDto): Promise<ProfessionEntity> {
     return this.professionService.updateProfession(id,updateProfessionDto);
   }
-
+  @UseGuards(JwtAuthGuard, AdminGuard)
   @Delete(':id')
   async delete(@Param('id',ParseIntPipe) id: number): Promise<ProfessionEntity> {
     return this.professionService.deleteProfession(id);
   }
+  @UseGuards(JwtAuthGuard, AdminGuard)
   @Patch('recover/:id')
   async  recover(@Param('id',ParseIntPipe) id: number): Promise<ProfessionEntity> {
     return this.professionService.recoverProfession(id);
