acb54424d9d2b7e0cd4ce2b38220fd5cdae4be60
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2024-05-06 19:51:35 +0100

    modifying jwt strategy to get it from the cookies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 import {
   ExcludeTimestampInterceptor
 } from './interceptors/exclude-timestamp-interceptor/exclude-timestamp-interceptor.interceptor';
@@ -8,7 +9,8 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist:true}));
-  app.useGlobalInterceptors(new ExcludeTimestampInterceptor())
+  app.useGlobalInterceptors(new ExcludeTimestampInterceptor());
+  app.use(cookieParser());
   app.enableCors({
     origin: (origin, callback) => callback(null, origin), // Reflect the origin in the CORS header
     credentials: true
