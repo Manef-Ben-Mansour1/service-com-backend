79e8fdc7d479605369523b7f68365b584861b3be
Emna Dammak <emna.dammak@outlook.com>
2024-05-07 09:14:57 +0100

    added cors and auth guards to sse

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import {
 } from './interceptors/exclude-timestamp-interceptor/exclude-timestamp-interceptor.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:true});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',  
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({whitelist:true}));
   app.useGlobalInterceptors(new ExcludeTimestampInterceptor())
   await app.listen(3000);
--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Sse } from '@nestjs/common';
+import { Controller, Sse, UseGuards } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { Observable, fromEvent, map } from 'rxjs';
+import { User } from 'src/decorators/user.decorator';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { JwtAuthGuard } from 'src/user/guards/jwt-auth.guard';
 
 @Controller('notification')
 export class NotificationController {
@@ -8,11 +11,18 @@ export class NotificationController {
 
   
   @Sse('sse')
-  sse(): Observable<any> {
+  @UseGuards(JwtAuthGuard)
+  sse(
+    @User() user : UserEntity
+  ): Observable<any> {
     return fromEvent(this.eventEmitter, 'notification.created').pipe(
-      map((payload) => {
+      map((payload : any) => {
         console.log({ payload });
-        return { data: payload };
+        if(payload.user.id !== user.id){
+            return;
+          }
+          console.log({ payload });
+          return { data: payload };
       }),
     );
   }
